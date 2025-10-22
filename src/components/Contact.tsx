7d2ab6340a2ad1a0ccab456d1289b178dabd93b0
kalyanSadhukhan <150598203+kalyanSadhukhan@users.noreply.github.com>
2025-10-22 10:53:31 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from 'emailjs-com';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
